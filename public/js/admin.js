ea9838bff14e35b603df140702b2f0d5dd1a16d2
srikritib <srikriti@gmail.com>
2026-04-26 02:27:47 -0400

    admin.js coding error update

--- public/js/admin.js
+++ public/js/admin.js
@@ -13,19 +13,30 @@
     } else {
       document.getElementById("admin-warning").textContent = "Authenticated as admin.";
       const result = await api("/api/admin/users"); 
-      document.getElementById("admin-users").innerHTML = result.users
-      .map(
-        (entry) => `
-          <tr>
-            <td>${entry.id}</td>
-            <td>${entry.username}</td>
-            <td>${entry.role}</td>
-            <td>${entry.displayName}</td>
-            <td>${entry.noteCount}</td>
-          </tr>
-        `
-      )
-      .join("");
+      //using textcontent to eliminate innerHTML vulns, for XSS or attack input being read as code issue
+      const tar = document.getElementById("admin-users");
+      tar.textContent = "";
+      result.users.forEach((entry) => { //replace previous tr and td tags for document.createElement, textContent, and appendChild as td, tr show up as literal text with document function and html tags
+        const row = document.createElement("tr");
+        const idOpt = document.createElement("td");
+        idOpt.textContent = entry.id;
+        const username = document.createElement("td");
+        username.textContent = entry.username;
+        const role = document.createElement("td");
+        role.textContent = entry.role;
+        const displayname = document.createElement("td");
+        displayname.textContent = entry.displayName;
+        const notecount = document.createElement("td");
+        notecount.textContent = entry.noteCount;
+
+        row.appendChild(idOpt);
+        row.appendChild(username);
+        row.appendChild(role);
+        row.appendChild(displayname);
+        row.appendChild(notecount);
+
+        tar.appendChild(row);
+      });
     }
   } catch (error) {
     document.getElementById("admin-warning").textContent = error.message;
